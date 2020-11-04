7c36eb20c77b7060f9dd5c97eee7e1cfc07fde47
goonzaah <49066454+goonzaah@users.noreply.github.com>
2020-11-04 19:36:48 -0300

    Update Startup.cs

--- LinkExpenses/LinkExpenses/Startup.cs
+++ LinkExpenses/LinkExpenses/Startup.cs
@@ -36,8 +36,6 @@ namespace LinkExpenses
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
