1eca640bce2be4066df2324d1075a69ae3844039
goonzaah <goonzaah@users.noreply.github.com>
2020-11-04 19:04:27 -0300

    cambios

---
+++ LinkExpenses/LinkExpenses/Get/Models/LinkedProcessed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkExpenses.Get.Models
+{
+    public class LinkedProcessed
+    {
+        public List<Link> Links { get; set; }
+        public List<Entry> PendingEntries { get; set; }
+        public List<Egress> PendingEgresses { get; set; }
+
+    }
+}
